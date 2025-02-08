7ced7e6e7af3e0c74085afa740ebf83822a59473
Jarek <jnfrontend@gmail.com>
2025-02-08 18:21:59 +0000

    Wrapp gallery images in <a> tag dynamically and fix issue with image.src

--- unit-10/exercise-5-venobox-lightbox-advanced/js/main.js
+++ unit-10/exercise-5-venobox-lightbox-advanced/js/main.js
@@ -11,18 +11,22 @@
       // https://medium.com/@_DandyLyons/how-to-use-a-js-for-of-loop-with-an-index-a4675ed22351
 
       let imageLink = document.createElement("a");
-
-      // while (imageBox.childNodes.length > 0) {
-      //  imageLink.appendChild(imageBox.childNodes[0]);
-      // }
-
       const imageURL = imageBox.querySelector("img").src;
 
       imageLink.setAttribute("href", imageURL);
       imageLink.setAttribute("data-gall", "gallery01");
       imageLink.className = "image-box-link";
-      // imageBox.replaceChildren(imageLink);
-      imageBox.appendChild(imageLink);
+
+      // console.log(imageBox);
+
+      while (imageBox.childNodes.length > 0) {
+        imageLink.appendChild(imageBox.childNodes[0]); // Get child nodes of .image-box and append to new <a> tag 
+       }
+
+      console.log(imageLink);
+
+      imageBox.replaceChildren(imageLink);
+      // imageBox.appendChild(imageLink);
     }
 
     // Create new VenoBox object
