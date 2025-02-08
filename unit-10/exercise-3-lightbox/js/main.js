83c3347f07d85acc521770357a1c61045cae32b4
Jarek <jnfrontend@gmail.com>
2025-02-08 10:32:50 +0000

    Add dynamic navigation dots with event handler

--- unit-10/exercise-3-lightbox/js/main.js
+++ unit-10/exercise-3-lightbox/js/main.js
@@ -8,8 +8,33 @@
       images = document.querySelectorAll(".gImg"),
       showImg = lightBox.querySelector(".showImg img"),
       close = lightBox.querySelector(".close");
+  let lightBoxContent = lightBox.querySelector('.lightBox_content');   
+  let navigationDots = null;
 
   function lightbox() {
+    // start add navigation dots
+    let navigationDotsHtml = '';
+    for (let i=0; i<images.length; i++) {
+      navigationDotsHtml += '<span class="navigation-dot"></span>';
+    }
+
+    const navDotsFragment = new DocumentFragment(); // https://developer.mozilla.org/en-US/docs/Web/API/DocumentFragment
+    let navDotsDiv = document.createElement("div");
+    navDotsDiv.className = 'navigation-dot-container';
+    navDotsDiv.innerHTML = navigationDotsHtml;
+    navDotsFragment.append(navDotsDiv);
+    lightBoxContent.appendChild(navDotsFragment);
+    navigationDots = lightBoxContent.querySelectorAll('.navigation-dot');
+    // end add navigation dots
+    
+    // navigation dot event handler
+    for (const [index, navDot] of navigationDots.entries()) {
+      navDot.addEventListener("click", () => {
+        showImage(index);
+      });
+    }
+
+    // image event handler
     for (const [index, image] of images.entries()) { // https://medium.com/@_DandyLyons/how-to-use-a-js-for-of-loop-with-an-index-a4675ed22351
       image.addEventListener("click", () => {
         showImage(index);
@@ -22,23 +47,36 @@
       <a class="next">❯</a>        
     </div>`);
 
+    // previous button event handler
     lightBox.querySelector('.navigation-buttons .previous').addEventListener("click", (event => {
       if (currImage !== 0) {
         currImage -= 1;
         showImg.src = images[currImage].src;
+        makeNavigationDotActive(currImage);
       } 
     }));
+
+    // next button event handler
     lightBox.querySelector('.navigation-buttons .next').addEventListener("click", (event => {
       if (currImage !== (images.length-1)) {
         currImage += 1;
         showImg.src = images[currImage].src;
+        makeNavigationDotActive(currImage);
       }
     }));
 
+    // close event handler
     close.addEventListener('click', ()=>{
       lightBox.style.display = "none";
       body.style.overflow = "visible";
     });
+  }
+
+  function makeNavigationDotActive(imageIndex) {
+    for (let i=0; i<navigationDots.length; i++) {
+      navigationDots[i].className = navigationDots[i].className.replace(" active", "");
+    }
+    navigationDots[imageIndex].className += " active";
   }
 
   function showImage(imageIndex) {
@@ -46,6 +84,7 @@
     currImage = imageIndex;
     lightBox.style.display = "block";
     body.style.overflow = "hidden";
+    makeNavigationDotActive(currImage);
   }
 
   function init() {
@@ -53,66 +92,7 @@
   }
 
   window.addEventListener("load", (event => {
-      init();
+      init(); // Initialize the lightbox
   }));
 
 })();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// old code
-var slideIndex = 1;
-//showSlide(slideIndex);
-
-function openLightbox() {
-  document.getElementById('Lightbox').style.display = 'block';
-}
-
-function closeLightbox() {
-  document.getElementById('Lightbox').style.display = 'none';
-}
-
-function changeSlide(n) {
-	showSlide(slideIndex += n);
-}
-
-function toSlide(n) {
-	showSlide(slideIndex = n);
-}
-
-function showSlide(n) {
-
-  const slides = document.getElementsByClassName('slide');
-  let modalPreviews = document.getElementsByClassName('modal-preview');
-
-  if (n > slides.length) {
-    slideIndex = 1;	
-  }
-  
-  if (n < 1) {
-  	slideIndex = slides.length;
-  }
-
-  for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (let i = 0; i < modalPreviews.length; i++) {
-      modalPreviews[i].className = modalPreviews[i].className.replace(' active', '');
-  }
-  
-  slides[slideIndex - 1].style.display = 'block';
-  modalPreviews[slideIndex - 1].className += ' active';
-}
