99bda96e79ace1707ecbf294216452493b3c0531
Jarek <jnfrontend@gmail.com>
2025-02-08 10:08:43 +0000

    Wrapp swiper in initCarousel() function

--- unit-10/exercise-2-slideshow/js/main.js
+++ unit-10/exercise-2-slideshow/js/main.js
@@ -1,25 +1,34 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    speed: 400,
-  spaceBetween: 100,
-//   slidesPerView: 'auto',
-  effect: 'slide',
+// code is wrapped in an IIFE (Immediately Invoked Function Expression). See https://developer.mozilla.org/en-US/docs/Glossary/IIFE for more details
+//
+(() => {
+  // globals
+  function initCarousel() {
+    // Initialize Swiper
+    const swiper = new Swiper(".swiper", {
+      // Optional parameters
+      direction: "horizontal",
+      loop: true,
+      speed: 400,
+      spaceBetween: 100,
+      effect: "slide",
 
-  
-    // Pagination
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-        clickable: 'true',
-      dynamicBullets: true,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
+      // Pagination
+      pagination: {
+        el: ".swiper-pagination",
+        type: "bullets",
+        clickable: "true",
+        dynamicBullets: false,
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  }
+
+  window.addEventListener("load", (event) => {
+    initCarousel();
   });
+})();
