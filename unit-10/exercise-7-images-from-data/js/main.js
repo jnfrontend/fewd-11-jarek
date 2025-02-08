e797e292833985ae1536947d5a1d7c5bbbe12bed
Jarek <jnfrontend@gmail.com>
2025-02-08 19:33:39 +0000

    Add venobox library

--- unit-10/exercise-7-images-from-data/js/main.js
+++ unit-10/exercise-7-images-from-data/js/main.js
@@ -1,62 +1,75 @@
 // code is wrapped in an IIFE (Immediately Invoked Function Expression). See https://developer.mozilla.org/en-US/docs/Glossary/IIFE for more details
 //
 (() => {
-    // globals
-    const contentContainer = document.getElementById("content");
-    
-    function renderImages(images){
-        let imagesHtml ='';
+  // globals
+  const contentContainer = document.getElementById("content");
 
-        imagesHtml = '<div class="image-container">';
+  function renderImages(images) {
+    let imagesHtml = "";
 
-        for (let i in images.hits) {
-            imagesHtml += '<div class="image">';
-            imagesHtml += '<a title="Click to see a large version of this image" href="' + images.hits[i].largeImageURL + '">';
-            imagesHtml += '<img src="' + images.hits[i].webformatURL + '">';
-            imagesHtml += '</a>';
-            imagesHtml += '<div class="image-info">';
-            imagesHtml += '<div class="tags">';
-            let tagsArray = images.hits[i].tags.split(',');
-            for (j in tagsArray) {
-                let linkQuery = encodeURI(tagsArray[j].trim() );
-                let linkText = tagsArray[j].trim();
-                imagesHtml += '<a href="#" onclick="loadImages(\'' +  linkQuery + '\')">' + linkText + '</a>'; 
-            }
-            imagesHtml += '</div>';
-            imagesHtml += '<div class="right">';
-            imagesHtml += '  <span class="favorites">' + images.hits[i].favorites + '</span>';
-            imagesHtml += '  <span class="likes">' + images.hits[i].likes + '</span>';
-            imagesHtml += '  <span class="comments">' + images.hits[i].comments + '</span>';
-            imagesHtml += '</div></div></div>';
-        }
-        imagesHtml += '</div>';
+    imagesHtml = '<div class="image-container">';
 
-        contentContainer.innerHTML = imagesHtml;
+    for (let i in images.hits) {
+      imagesHtml += `<div class="image">
+                <a title="Click to see a large version of this image" href="${images.hits[i].largeImageURL}" class="image-box-link" data-gall="gallery01">
+                    <img src="${images.hits[i].webformatURL}">
+                </a>
+                <div class="image-info">
+                    <div class="tags">`;
+      let tagsArray = images.hits[i].tags.split(",");
+      for (j in tagsArray) {
+        let linkQuery = encodeURI(tagsArray[j].trim());
+        let linkText = tagsArray[j].trim();
+        imagesHtml += `<a href="#" onclick="loadImages('${linkQuery} ')">${linkText}</a>`;
+
+      }
+      imagesHtml += `</div>
+            <div class="right">
+                <span class="favorites">${images.hits[i].favorites}</span>
+                <span class="likes">${images.hits[i].likes}</span>
+                <span class="comments">${images.hits[i].comments}</span>
+            </div></div></div>`;
     }
+    imagesHtml += "</div>";
 
-    function init() {
-        try {
-            // option 1 - load data as a JB object
-            console.log('dataOption1');
-            console.log(dataOption1);
-            renderImages(dataOption1); // render images
-            // option 2 - load data as a string variable
-            const dataOption2 = JSON.parse(dataStringOption2);
-            console.log('dataOption2');
-            console.log(dataOption2);
-            //renderImages(dataOption2); // render images    
-            // option 3 - embed data in html
-            const dataOption3 = JSON.parse(document.getElementById('dataStringOption3').text);
-            console.log('dataOption3');
-            console.log(dataOption3)
-        } catch(err) {
-            console.error(err);
-            contentContainer.innerHTML = '<h2>Error</h2><p>No images to display.</p><p>' + err + '</p>';
-        }
+    contentContainer.innerHTML = imagesHtml;
+  }
+
+  function init() {
+    try {
+      // option 1 - load data as a JB object
+      console.log("dataOption1");
+      console.log(dataOption1);
+      renderImages(dataOption1); // render images
+      // option 2 - load data as a string variable
+      const dataOption2 = JSON.parse(dataStringOption2);
+      console.log("dataOption2");
+      console.log(dataOption2);
+      //renderImages(dataOption2); // render images
+      // option 3 - embed data in html
+      const dataOption3 = JSON.parse(
+        document.getElementById("dataStringOption3").text
+      );
+      console.log("dataOption3");
+      console.log(dataOption3);
+
+    // create venoBox lightbox and initialise
+    new VenoBox({
+        selector: ".image-box-link",
+        numeration: true,
+        infinigall: true,
+        share: true,
+        spinner: "rotating-plane",
+    });
+
+    } catch (err) {
+      console.error(err);
+      contentContainer.innerHTML =
+        "<h2>Error</h2><p>No images to display.</p><p>" + err + "</p>";
     }
+  }
 
-    window.addEventListener("load", (event => {
-        init();
-    }));
-
+  window.addEventListener("load", (event) => {
+    init();
+  });
 })();
