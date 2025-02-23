b3d7b4f01e8be4b6ee3510c8b82cb93fc5e7cd94
Jarek <jnfrontend@gmail.com>
2025-02-23 11:42:04 +0000

    Add class DataTable, private properties and constructor with argument (data)

--- unit-12/exercise-2-class-table-from-data/js/main.js
+++ unit-12/exercise-2-class-table-from-data/js/main.js
@@ -1,13 +1,21 @@
 // code is wrapped in an IIFE (Immediately Invoked Function Expression). See https://developer.mozilla.org/en-US/docs/Glossary/IIFE for more details
 //
 (() => {
-    // globals
-    const contentContainer = document.getElementById("content");
-    
-    function renderTable(publicHolidays){
-        let hols = publicHolidays;
-        let outputHtml = `<div class="table-container">
-            <h2>List of Public Holidays in Ireland for 2024</h2>
+  class DataTable {
+    // Properties
+    data;
+    contentContainer;
+
+    constructor(data) {
+      this.data = data;
+      this.contentContainer = document.getElementById("content");
+    }
+
+    render() {
+      let hols = this.data;
+      let outputHtml = `
+        <div class="table-container">
+            <h2>List of Public Holidays in Ireland for 2025</h2>
                 <table class="blueTable">
                     <thead>
                         <tr>
@@ -20,35 +28,38 @@
                     </thead>
                     <tbody>`;
 
-        for (let i in hols) {
-            outputHtml += `<tr>
-                <td>${ hols[i].date }</td>
-                <td>${ hols[i].localName }</td>
-                <td>${ hols[i].name }</td>
-                <td>${ hols[i].countryCode }</td>
-                <td>${ hols[i].types }</td>
+      for (let i in hols) {
+        console.log('Hols-td-' + [i], hols[i]);
+        outputHtml += `<tr>
+                <td>${hols[i].date}</td>
+                <td>${hols[i].localName}</td>
+                <td>${hols[i].name}</td>
+                <td>${hols[i].countryCode}</td>
+                <td>${hols[i].types}</td>
             </tr>`;
-        }
-        outputHtml += `</tbody>
+      }
+      outputHtml += `</tbody>
             </table>
         </div>`;
 
-        contentContainer.innerHTML = outputHtml;
+      this.contentContainer.innerHTML = outputHtml; // Display holidays data in the container <table>
     }
+  } // END Class: DataTable
 
-    function init() {
-        try {
-            // load data as a JS object
-            console.log(data);
-            renderTable(data); // render table
-        } catch(err) {
-            console.error(err);
-            contentContainer.innerHTML = '<h2>Error</h2><p>No public holidays to display.</p><p>' + err + '</p>';
-        }
+  function init() {
+    try {
+      // Load data as a JS object
+      console.log('Data is: ', data);
+      const dataTable = new DataTable(data);
+      console.log(dataTable);
+      dataTable.render(); // Render table
+    } catch (err) {
+      console.error(err);
+      contentContainer.innerHTML = "<h2>Error</h2><p>No public holidays to display.</p><p>" + err + "</p>";
     }
+  }
 
-    window.addEventListener("load", (event => {
-        init();
-    }));
-
-})();
+  window.addEventListener("load", (event) => {
+    init();
+  });
+})(); // END EventListener
