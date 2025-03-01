f117b71830593f8315ca566d30910e0e903c8615
Jarek <jnfrontend@gmail.com>
2025-03-01 13:17:27 +0000

    Create generic DataTable class with API_URL and container arguments

--- unit-12/exercise-5-generic-table-using-fetch/js/main.js
+++ unit-12/exercise-5-generic-table-using-fetch/js/main.js
@@ -5,21 +5,22 @@
   class DataTable {
     // Properties
     data;
+    API_URL;
     contentContainer;
-    apiProductsUrl;
 
-    constructor(apiProductsUrl) {
-      // this.data = data;
-      this.apiProductsUrl = apiProductsUrl;
-      this.contentContainer = document.getElementById("content");
-      this.loadProductData(); // Init fetch()
+    constructor(API_URL, contentContainer) {
+      this.data = [];
+      this.API_URL = API_URL;
+      this.contentContainer = contentContainer;
+      // this.contentContainer = document.getElementById("content");
+      this.loadData(); // Init fetch()
     }
 
-    async loadProductData() {
+    async loadData() {
       try {
         // After this line, our function will wait for the `fetch()` call to be settled
         // The `fetch()` call will either return a Response or throw an error
-        const response = await fetch(this.apiProductsUrl);
+        const response = await fetch(this.API_URL);
         if (!response.ok) {
           throw new Error(`HTTP error: ${response.status}`);
         }
@@ -28,17 +29,15 @@
         // After this line, our function will wait for the `response.json()` call to be settled
         // The `response.json()` call will either return the parsed JSON object or throw an error
         const decodedJsonData = await response.json();
-        console.log(decodedJsonData);
+        // console.log(decodedJsonData);
 
         this.data = decodedJsonData; // Update the data property with the fetched data
-        this.render();
-
-        return decodedJsonData; // Return the decoded JSON data for further processing
+        this.render(); // Pass the response(decodedJsonData) to the render() function
       } catch (error) {
-        console.error(`Could not get products: ${error}`);
+        console.error(`Could not get data: ${error}`);
         this.contentContainer.innerHTML = `
           <h2>Error</h2>
-          <p>No products to display.</p>
+          <p>No data to display.</p>
           <p>${error.message}</p>
         `;
       }
@@ -46,61 +45,60 @@
 
     render() {
       if (!this.data || this.data.length === 0) {
-        this.contentContainer.innerHTML = "<p>No products data available to display.</p>";
+        this.contentContainer.innerHTML =
+          "<p>No data available to display.</p>";
         return;
       }
 
-      const apiAssetsImagesUrl = "https://raw.githubusercontent.com/mdn/learning-area/main/javascript/apis/fetching-data/can-store/images/";
+      console.log('Render this data: ', this.data);
 
-      let products = this.data;
+      // Get the property names of the first object in the data array
+      const objPropertyNameHeaders = Object.getOwnPropertyNames(this.data[0]);
+
+      // Create table with json data properties
       let outputHtml = `
         <div class="table-container">
-            <h2>List of Products</h2>
+            <h2>Table Heading</h2>
                 <table class="greenTable">
                     <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th align="center">Price</th>
-                            <th align="center">Image</th>
-                            <th>Type</th>
-                        </tr>
-                    </thead>
-                    <tbody>`;
+                        <tr>`;
 
-      for (let i in products) {
-        console.log("Products-td-" + [i], products[i]);
-  
-        // Create image path for the product
-        const productImagePath = `${apiAssetsImagesUrl}${products[i].image}`;
-        console.log("Product image url is: ", productImagePath);
-
-        outputHtml += `<tr>
-                <td>${products[i].name}</td>
-                <td align="center"><span class="currency">&#x20AC;</span> ${products[i].price}</td>
-                <td align="center" class="product_img"><img src="${productImagePath}"></td>
-                <td>${products[i].type}</td>
-            </tr>`;
+      let tableHeaders = "";
+      for (let header in objPropertyNameHeaders) {
+        tableHeaders += `<th>${objPropertyNameHeaders[header]}</th>`;
       }
-      outputHtml += `</tbody>
-            </table>
-        </div>`;
 
-      this.contentContainer.innerHTML = outputHtml; // Display products data in the container <table>
+      outputHtml += tableHeaders;
+      outputHtml += `</tr></thead><tbody>`; // Close table headers(with object property names) and oppen table body
+
+      for (let i in this.data) {
+        let tableItem = this.data[i];
+        console.log("DATA Object - Table Column Items", tableItem);
+
+        let tableRow = "<tr>";
+        for (let header in objPropertyNameHeaders) {
+          let tableColumnName = objPropertyNameHeaders[header];
+          // console.log("Table Header Name: ", tableColumnName);
+          tableRow += `<td>${String(tableItem[tableColumnName]) || ""}</td>`;
+        }
+        tableRow += "</tr>";
+
+        outputHtml += tableRow;
+      }
+
+      outputHtml += `</tbody></table></div>`;
+      this.contentContainer.innerHTML += outputHtml; // Display table with data in a container ()
     }
   } // END Class: DataTable
 
   function init() {
-    try {
-      const productsDataUrl = "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json";
+    const tableContainer = document.getElementById("content");
+    const productsDataUrl = "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json";
+    const holidaysDataUrl = "https://date.nager.at/api/v3/PublicHolidays/2025/IE";
 
-      // Init DataTable
-      const dataTable = new DataTable(productsDataUrl);
-    } catch (err) {
-      console.error(err);
-      const contentContainer = document.getElementById("content");
-      contentContainer.innerHTML =
-        "<h2>Error</h2><p>No products to display.</p><p>" + err + "</p>";
-    }
+    // Init DataTable
+    new DataTable(holidaysDataUrl, tableContainer);
+    new DataTable(productsDataUrl, tableContainer);
   }
 
   window.addEventListener("load", (event) => {
