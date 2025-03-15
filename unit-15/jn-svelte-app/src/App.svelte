9b81105ac16802efe590c7757a0cd6d393c79fcc
Jarek <jnfrontend@gmail.com>
2025-03-15 20:32:25 +0000

    Add increment function on click event

--- unit-15/jn-svelte-app/src/App.svelte
+++ unit-15/jn-svelte-app/src/App.svelte
@@ -1,14 +1,32 @@
 <script>
 	export let name;
+
+	let count = 10;
+	function increment() {
+		console.log("increment");
+		count += 1;
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<header>
+		<h1><b>Unit-15</b><br>Svelte App</h1>
+	</header>
+	<h2>Hello {name}!</h2>
+	<p>Count: {count}</p>
+	<button on:click={increment}>Click to Increment</button>
 </main>
+<br>
+<hr>
+<footer>
+	<small>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.
+	</small>
+</footer>
 
 <style>
-	main {
+	main,
+	footer {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
@@ -20,6 +38,17 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	button {
+		background-color: #ff3e00;
+		padding: 18px 36px;
+		border-radius: 8px;
+		margin:  0 auto;
+		color: white;
+		cursor: pointer;
+		display: block;
+		border: none;
 	}
 
 	@media (min-width: 640px) {
--- unit-15/jn-svelte-app/src/main.js
+++ unit-15/jn-svelte-app/src/main.js
@@ -1,9 +1,10 @@
 import App from './App.svelte';
 
+// SPA (Single-page application)
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'world'
+		name: 'Jarek'
 	}
 });
 
